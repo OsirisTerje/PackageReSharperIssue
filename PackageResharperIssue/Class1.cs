b202eb53751ba4fadec97a614a98cf3c50fd887b
Terje Sandstrom <terje@osiris.no>
2013-11-19 13:57:17 -0800

    Initial code

---
+++ PackageResharperIssue/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+namespace PackageResharperIssue
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void AddRandom()
+        {
+            var gen = new Mock<IRandom>();
+            gen.Setup(f => f.GenerateRandomNumber()).Returns(42);
+
+            var result = gen.Object.GenerateRandomNumber();
+            Assert.That(result,Is.EqualTo(42));
+        }
+    }
+}
